a6eba23a81c53964c0181fe1b793c19203e5646d
Los Altos <personapage@pm.me>
2026-08-05 23:30:51 +0300

    Redesign nanoblogs page

---
+++ nanoblogs/script.js
@@ -0,0 +1,27 @@
+function timeAgo(date) {
+    const seconds = Math.floor((Date.now() - date.getTime()) / 1000);
+    if (seconds < 60) return "· now";
+    const intervals = [
+        { label: "year", seconds: 31536000 },
+        { label: "month", seconds: 2592000 },
+        { label: "week", seconds: 604800 },
+        { label: "day", seconds: 86400 },
+        { label: "hour", seconds: 3600 },
+        { label: "min", seconds: 60 }
+    ];
+
+    for (const interval of intervals) {
+        const count = Math.floor(seconds / interval.seconds);
+        if (count >= 1) {
+            return `· ${count} ${interval.label}${count > 1 ? "s" : ""} ago`;
+        }
+    }
+}
+
+function updateTimeAgo() {
+    document.querySelectorAll(".nanoblog-post-time").forEach(e => {
+        e.textContent = timeAgo(new Date(e.dateTime));
+    });
+}
+updateTimeAgo();
+setInterval(updateTimeAgo, 10000);
